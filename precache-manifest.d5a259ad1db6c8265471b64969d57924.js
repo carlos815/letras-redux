f8f28b9ad2b815d515da2d454354db5b8ddfb890
Carlos Hernández <carlos_t815@outlook.com>
2020-08-24 11:35:40 -0400

    Updates

---
+++ precache-manifest.d5a259ad1db6c8265471b64969d57924.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e8e05dea0b8fb978de3f6c5426bd70",
+    "url": "/letras-redux/index.html"
+  },
+  {
+    "revision": "6c418bd90f1b9f0072e7",
+    "url": "/letras-redux/static/css/main.535ae164.chunk.css"
+  },
+  {
+    "revision": "af918c9cf34eefd2a4e5",
+    "url": "/letras-redux/static/js/2.80267eef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/letras-redux/static/js/2.80267eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c418bd90f1b9f0072e7",
+    "url": "/letras-redux/static/js/main.659b9d3d.chunk.js"
+  },
+  {
+    "revision": "57422a30c263b33ec0ce",
+    "url": "/letras-redux/static/js/runtime-main.62b469d3.js"
+  },
+  {
+    "revision": "1d32ccd718e7e89c758f4a92d0201eaa",
+    "url": "/letras-redux/static/media/logo.1d32ccd7.svg"
+  },
+  {
+    "revision": "af3b13acb58288ea585bd0280e5cb46f",
+    "url": "/letras-redux/static/media/magnifier-glass.af3b13ac.svg"
+  },
+  {
+    "revision": "e9c9f64d480b1f09088597876c8fd8b5",
+    "url": "/letras-redux/static/media/no-cover.e9c9f64d.svg"
+  }
+]);
